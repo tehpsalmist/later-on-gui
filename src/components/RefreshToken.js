4e05bb110f467b5cc198118d6730c23700a7bf83
tehpsalmist <bstewardcodes@gmail.com>
2019-11-02 13:14:43 -0400

    simplify copying the token

--- src/components/RefreshToken.js
+++ src/components/RefreshToken.js
@@ -3,36 +3,34 @@ import { useAuth0 } from '../auth'
 
 export const RefreshToken = ({ className }) => {
   const { refreshToken, getRefreshToken } = useAuth0()
-  const [hidden, setHidden] = useState(true)
   const [copied, setCopied] = useState(false)
 
   const copy = () => {
     if (refreshToken) {
-      navigator.clipboard.writeText(refreshToken)
-        .then(() => setCopied(true))
+      const range = document.createRange()
+      range.selectNode(document.querySelector('#refresh-token'))
+      window.getSelection().addRange(range)
+
+      try {
+        const success = document.execCommand('copy')
+
+        if (success) {
+          setCopied(true)
+        }
+
+        window.getSelection().removeAllRanges()
+      } catch (e) {
+        console.error('unable to copy', e)
+      }
     }
   }
-
-  useEffect(() => {
-    navigator.clipboard.readText()
-      .then(clipText => {
-        if (clipText && clipText === refreshToken) {
-          setCopied(true)
-        } else if (copied && clipText !== refreshToken) {
-          setCopied(false)
-        }
-      })
-  }, [copied, refreshToken])
 
   return <div className={className}>
     <h3 className='text-xl text-center'>Refresh Token:</h3>
     {refreshToken
       ? <div className='flex justify-between items-center min-w-64 rounded p-2 border border-green-300'>
-        <pre className='truncate mr-auto'>{hidden ? '************' : refreshToken}</pre>
+        <pre id='refresh-token' className='truncate mr-auto'>{refreshToken}</pre>
         <button className='rounded-full bg-gray-300 text-green-500 min-h-12 min-w-12 flex-center shadow-md ml-4 cursor-pointer hover:bg-gray-400 focus:outline-none' onClick={e => copy()}>Copy</button>
-        <button className='rounded-full bg-gray-300 text-green-500 min-h-12 min-w-12 flex-center shadow-md ml-1 cursor-pointer hover:bg-gray-400 focus:outline-none' onClick={e => setHidden(!hidden)}>
-          {hidden ? 'Show' : 'Hide'}
-        </button>
       </div>
       : <button
         className='p-2 rounded bg-blue-300 text-gray-600 shadow-md hover:bg-blue-400 hover:text-gray-700 focus:bg-blue-500 focus:text-white'

4b1641297254c4aa6c0d7b35a75a591357c3d1e6
tehpsalmist <bstewardcodes@gmail.com>
2019-11-09 19:43:06 -0500

    access token

---
+++ src/components/AccessToken.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { useAuth0 } from '../auth'
+
+export const AccessToken = ({ className }) => {
+  const { authToken } = useAuth0()
+  const [copied, setCopied] = useState(false)
+
+  const copy = () => {
+    if (authToken) {
+      const range = document.createRange()
+      range.selectNodeContents(document.querySelector('#access-token'))
+      window.getSelection().addRange(range)
+
+      try {
+        const success = document.execCommand('copy')
+
+        if (success) {
+          setCopied(true)
+        }
+
+        window.getSelection().removeAllRanges()
+      } catch (e) {
+        console.error('unable to copy', e)
+      }
+    }
+  }
+
+  return <div className={className}>
+    <h3 className='text-xl text-center'>Access Token:</h3>
+    {authToken
+      ? <div className='flex justify-between items-center max-w-64 md:max-w-sm rounded p-2 border border-green-300'>
+        <pre id='access-token' className='truncate mr-auto'>{authToken}</pre>
+        <button className='rounded-full bg-gray-300 text-green-500 min-h-12 min-w-12 flex-center shadow-md ml-4 cursor-pointer hover:bg-gray-400 focus:outline-none' onClick={e => copy()}>Copy</button>
+      </div>
+      : 'Fetching Latest Token...'}
+    {copied && <p className='text-center text-green-500'>Copied!</p>}
+  </div>
+}
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useAuth0 } from '../auth'
-import { RefreshToken } from '.'
+import { RefreshToken, AccessToken } from '.'
 
 export const Profile = props => {
   const { loading, user, getRefreshToken, refreshToken } = useAuth0()
@@ -10,6 +10,7 @@ export const Profile = props => {
   return <main className='flex-center flex-col'>
     <img className='rounded-full max-w-32 md:max-w-64' src={user.picture} />
     <h2 className='text-3xl'>{user.name || user.nickname || user.given_name || user.email || 'No Name'}</h2>
+    <AccessToken className='mt-12 mx-2' />
     <RefreshToken className='mt-12 mx-2' />
   </main>
 }

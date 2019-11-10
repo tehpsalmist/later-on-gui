f473c12767b98c0911522d2e30c6a0beb1839414
tehpsalmist <bstewardcodes@gmail.com>
2019-11-10 02:54:54 -0500

    just the perfect spacing between those emojis

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,10 @@ import React from 'react'
 export const Home = props => {
   return <main className='flex-center flex-col'>
     <h1 className='text-2xl md:text-4xl font-semibold'>🐌 Later On 🚀</h1>
-    <h4 className='md:text-lg'><em>💻 Programmatically Procrastinated Processing 🖥</em></h4>
+    <h4 className='md:text-lg'>
+      💻{' '}
+      <em>Programmatically Procrastinated Processing</em>
+      {' '}🖥
+    </h4>
   </main>
 }

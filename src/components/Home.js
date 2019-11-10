dee43df7b4113123b78f06952fdc7eb595ac0432
tehpsalmist <bstewardcodes@gmail.com>
2019-11-10 02:00:24 -0500

    some title stuff

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 export const Home = props => {
-  return <main>
-    Welcome to Later On!
+  return <main className='flex-center flex-col'>
+    <h1 className='text-2xl md:text-4xl font-semibold'>Later On</h1>
+    <h4 className='md:text-lg'><em>Programmatically Procrastinated Processing</em></h4>
   </main>
 }

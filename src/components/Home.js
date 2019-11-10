41d1f976d0b9b2af48d840f66ed020e1bac057d6
tehpsalmist <bstewardcodes@gmail.com>
2019-11-10 02:23:28 -0500

    emojis to test CI

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Home = props => {
   return <main className='flex-center flex-col'>
-    <h1 className='text-2xl md:text-4xl font-semibold'>Later On</h1>
+    <h1 className='text-2xl md:text-4xl font-semibold'>🐌 Later On 🚀</h1>
     <h4 className='md:text-lg'><em>Programmatically Procrastinated Processing</em></h4>
   </main>
 }

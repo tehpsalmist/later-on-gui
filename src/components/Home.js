a435ce35dbbcdf8785611444251d76c8902dbb56
tehpsalmist <bstewardcodes@gmail.com>
2019-11-10 02:46:34 -0500

    ci testing.....

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,6 @@ import React from 'react'
 export const Home = props => {
   return <main className='flex-center flex-col'>
     <h1 className='text-2xl md:text-4xl font-semibold'>🐌 Later On 🚀</h1>
-    <h4 className='md:text-lg'><em>Programmatically Procrastinated Processing</em></h4>
+    <h4 className='md:text-lg'><em>💻 Programmatically Procrastinated Processing 🖥</em></h4>
   </main>
 }

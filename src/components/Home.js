ca85a36821ae681a7093f564b7c66d1c43484826
tehpsalmist <bstewardcodes@gmail.com>
2019-11-10 15:51:46 -0500

    fixed the emojis

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,9 @@ import React from 'react'
 
 export const Home = props => {
   return <main className='flex-center flex-col'>
-    <h1 className='text-2xl md:text-4xl font-semibold'>🐌 Later On 🚀</h1>
+    <h1 className='text-2xl md:text-4xl font-semibold'>🕙 Later On 🚀</h1>
     <h4 className='md:text-lg'>
-      💻{' '}
       <em>Programmatically Procrastinated Processing</em>
-      {' '}🖥
     </h4>
   </main>
 }

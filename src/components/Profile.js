ea89797f816495a496c788c2be3f93f9ab495ce0
tehpsalmist <bstewardcodes@gmail.com>
2019-11-24 20:42:28 -0500

    prioritize nickname and show email

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,7 +9,8 @@ export const Profile = props => {
 
   return <main className='flex-center flex-col'>
     <img className='rounded-full max-w-32 md:max-w-64' src={user.picture} />
-    <h2 className='text-3xl'>{user.name || user.nickname || user.given_name || user.email || 'No Name'}</h2>
+    <h2 className='text-3xl'>{user.nickname || user.name || user.given_name || user.email || 'No Name'}</h2>
+    <h4 className='text-lg'>{user.email}</h4>
     <AccessToken className='mt-12 mx-2 max-w-64 md:max-w-sm' />
     <RefreshToken className='mt-12 mx-2 max-w-64 md:max-w-sm' />
   </main>

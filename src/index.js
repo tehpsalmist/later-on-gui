ca48cbbb73d27f1c1c1b72ea81e67bb31423434f
tehpsalmist <bstewardcodes@gmail.com>
2019-12-26 00:45:27 -0500

    test out env

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Auth0Provider } from './auth'
 import { BrowserRouter } from 'react-router-dom'
-import { App } from './components'
+
+console.log(process.env.BASE_URL)
 
 render(
   <BrowserRouter>

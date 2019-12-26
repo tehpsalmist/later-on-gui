fbee77db2e0b3cf4cbc7b9b5b044d7e29fed5ff6
tehpsalmist <bstewardcodes@gmail.com>
2019-12-25 22:29:16 -0500

    better copying maybe

--- src/components/AccessToken.js
+++ src/components/AccessToken.js
@@ -1,15 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { useAuth0 } from '../auth'
 
 export const AccessToken = ({ className }) => {
+  const inputRef = useRef()
   const { authToken } = useAuth0()
   const [copied, setCopied] = useState(false)
 
-  const copy = () => {
+  const copy = e => {
     if (authToken) {
-      const range = document.createRange()
-      range.selectNodeContents(document.querySelector('#access-token'))
-      window.getSelection().addRange(range)
+      inputRef.current && inputRef.current.select()
 
       try {
         const success = document.execCommand('copy')
@@ -18,7 +17,7 @@ export const AccessToken = ({ className }) => {
           setCopied(true)
         }
 
-        window.getSelection().removeAllRanges()
+        e.target.focus()
       } catch (e) {
         console.error('unable to copy', e)
       }
@@ -29,8 +28,8 @@ export const AccessToken = ({ className }) => {
     <h3 className='text-xl text-center'>Access Token:</h3>
     {authToken
       ? <div className='flex justify-between items-center rounded p-2 border border-green-300'>
-        <pre id='access-token' className='truncate mr-auto'>{authToken}</pre>
-        <button className='rounded-full bg-gray-300 text-green-500 min-h-12 min-w-12 flex-center shadow-md ml-4 cursor-pointer hover:bg-gray-400 focus:outline-none' onClick={e => copy()}>Copy</button>
+        <input ref={inputRef} className='w-full truncate mr-auto font-mono focus:outline-none' value={authToken} readOnly />
+        <button className='rounded-full bg-gray-300 text-green-500 min-h-12 min-w-12 flex-center shadow-md ml-4 cursor-pointer hover:bg-gray-400 focus:outline-none' onClick={e => copy(e)}>Copy</button>
       </div>
       : 'Fetching Latest Token...'}
     {copied && <p className='text-center text-green-500'>Copied!</p>}
--- src/components/RefreshToken.js
+++ src/components/RefreshToken.js
@@ -1,15 +1,14 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useRef } from 'react'
 import { useAuth0 } from '../auth'
 
 export const RefreshToken = ({ className }) => {
+  const inputRef = useRef()
   const { refreshToken, getRefreshToken } = useAuth0()
   const [copied, setCopied] = useState(false)
 
-  const copy = () => {
+  const copy = e => {
     if (refreshToken) {
-      const range = document.createRange()
-      range.selectNodeContents(document.querySelector('#refresh-token'))
-      window.getSelection().addRange(range)
+      inputRef.current && inputRef.current.select()
 
       try {
         const success = document.execCommand('copy')
@@ -18,7 +17,7 @@ export const RefreshToken = ({ className }) => {
           setCopied(true)
         }
 
-        window.getSelection().removeAllRanges()
+        e.target.focus()
       } catch (e) {
         console.error('unable to copy', e)
       }
@@ -29,8 +28,8 @@ export const RefreshToken = ({ className }) => {
     <h3 className='text-xl text-center'>Refresh Token:</h3>
     {refreshToken
       ? <div className='flex justify-between items-center rounded p-2 border border-red-300'>
-        <pre id='refresh-token' className='truncate mr-auto'>{refreshToken}</pre>
-        <button className='rounded-full bg-gray-300 text-red-500 min-h-12 min-w-12 flex-center shadow-md ml-4 cursor-pointer hover:bg-gray-400 focus:outline-none' onClick={e => copy()}>Copy</button>
+        <input ref={inputRef} className='w-full truncate mr-auto font-mono focus:outline-none' value={refreshToken} readOnly />
+        <button className='rounded-full bg-gray-300 text-red-500 min-h-12 min-w-12 flex-center shadow-md ml-4 cursor-pointer hover:bg-gray-400 focus:outline-none' onClick={e => copy(e)}>Copy</button>
       </div>
       : <button
         className='p-2 rounded bg-blue-300 text-gray-600 shadow-md hover:bg-blue-400 hover:text-gray-700 focus:bg-blue-500 focus:text-white'

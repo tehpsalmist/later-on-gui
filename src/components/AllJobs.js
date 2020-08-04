5bdbf654f5aea27a222328ea6817913df094eb9b
tehpsalmist <bstewardcodes@gmail.com>
2020-08-04 03:29:38 -0400

    feat: total jobs badge

--- src/components/AllJobs.js
+++ src/components/AllJobs.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import useSWR, { mutate, trigger } from 'swr'
 import { cronDescriptionOrFormattedDate } from '../utils'
 import { useAuth0 } from '../auth'
@@ -6,15 +6,21 @@ import { baseURL } from './SWRWrapper'
 import { useFreshClosure } from '../hooks'
 import { JobItem } from './JobItem'
 
-export const AllJobs = props => {
+export const AllJobs = ({ setJobs }) => {
   const { authToken } = useAuth0()
   const { data, error } = useSWR('/jobs')
+
+  const { jobs, totalJobs = 0 } = (data || {})
+
+  useEffect(() => {
+    if (data) {
+      setJobs(totalJobs)
+    }
+  }, [totalJobs])
 
   if (error) return <p className='text-red-600'>{error.message}</p>
 
   if (!data) return <p className='text-green-500'>Loading...</p>
-
-  const { jobs } = data
 
   if (!jobs.length) return <p className='text-green-500'>No jobs created yet.</p>
 
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs'
 import 'react-tabs/style/react-tabs.css'
 import { AllJobs, EditJob } from '.'
@@ -6,15 +6,23 @@ import { useRememberedState } from '../hooks'
 
 export const Dashboard = props => {
   const [defaultIndex, setDefaultIndex] = useRememberedState(0)
+  const [totalJobs, setTotalJobs] = useState(0)
 
   return <main>
     <Tabs defaultIndex={defaultIndex} forceRenderTabPanel className='m-2 md:m-8 overflow-y-scroll max-h-full'>
       <TabList>
-        <Tab onClick={e => setDefaultIndex(0)}>Existing Jobs</Tab>
+        <Tab onClick={e => setDefaultIndex(0)}>
+          Existing Jobs
+          {totalJobs
+            ? <span className='rounded-full bg-blue-500 text-white inline-flex justify-center px-2 ml-2'>
+              {totalJobs}
+            </span>
+            : null}
+        </Tab>
         <Tab onClick={e => setDefaultIndex(1)}>Create A Job</Tab>
       </TabList>
       <TabPanel>
-        <AllJobs />
+        <AllJobs setJobs={setTotalJobs} />
       </TabPanel>
       <TabPanel>
         <EditJob />
